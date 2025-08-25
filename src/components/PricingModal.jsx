6b4c5d1016cb632425696f57746dc3c1b4b52bea
RealKien <letrungkien208@gmail.com>
2025-08-25 12:38:30 -0400

    feat(pricing-modal): add top-right close (X) button that triggers onClose; prevent overlay click propagation

--- src/components/PricingModal.jsx
+++ src/components/PricingModal.jsx
@@ -110,7 +110,17 @@ const PricingModal = ({ onPlanSelect, onClose, showUpgradeMessage = false, onBoo
 
   const modalContent = (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <div className="modal-content relative" onClick={(e) => e.stopPropagation()}>
+        {/* Close (X) button */}
+        <button
+          aria-label="Close pricing modal"
+          onClick={onClose}
+          className="absolute top-3 right-3 text-gray-400 hover:text-gray-600 transition-colors p-1 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+        >
+          <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
+        </button>
         <div className="p-6">
           {/* Upgrade Required Message */}
           {showUpgradeMessage && (
